84256b9957e865a73e66f3a9450e5a12981bc29b
catthug92 <itnikitins@gmail.com>
2023-08-17 17:40:09 +0300

    add new viewModels

--- MVVM+Coordinator/ViewModels/LoginViewModel.swift
+++ MVVM+Coordinator/ViewModels/LoginViewModel.swift
@@ -6,23 +6,75 @@
 //
 
 import Foundation
-import UIKit.UIColor
+import Combine
+//import UIKit.UIColor
+
+enum ViewStates {
+    case loading
+    case success
+    case failed
+    case none
+}
 
 class LoginViewModel {
-    // Dynamic это пользовательский класс который занимается биндигом интерфейса и модели данных
-    var statusText = Dynamic("")
-    var statusColor = Dynamic(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-    var isLoggedIn: Bool = false
+    @Published var email = ""
+    @Published var password = ""
+    @Published var state: ViewStates = .none
     
-    func userButtonPressed(login: String, password: String) {
-        if login != User.logins[0].login || password != User.logins[0].password {
-            statusText.value = "Log in failed."
-            statusColor.value = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-            isLoggedIn = false
-        } else {
-            statusText.value = "You successfully logged in."
-            statusColor.value = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-            isLoggedIn = true
+    var isValidEmailPublisher: AnyPublisher<Bool, Never> {
+        $email
+            .map { $0.isEmail() }
+            .eraseToAnyPublisher()
+    }
+    
+    var isValidPasswordPublisher: AnyPublisher<Bool, Never> {
+        $password
+            .map { !$0.isEmpty }
+            .eraseToAnyPublisher()
+    }
+    
+    var isLoginEnabled: AnyPublisher<Bool, Never> {
+        Publishers.CombineLatest(isValidEmailPublisher, isValidPasswordPublisher)
+            .map { $0 && $1 }
+            .eraseToAnyPublisher()
+    }
+    
+    
+    // отправка запроса на сервер
+    func submitLogin() {
+        state = .loading
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            guard let self = self else { return }
+            if self.isCorrectLogin() {
+                state = .success
+            } else {
+                state = .failed
+            }
         }
     }
+    
+    func isCorrectLogin() -> Bool {
+        return email == "test@mail.com" && password == "12345"
+    }
 }
+
+
+//class LoginViewModel {
+//    // Dynamic это пользовательский класс который занимается биндигом интерфейса и модели данных
+//    var statusText = Dynamic("")
+//    var statusColor = Dynamic(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+//    var isLoggedIn: Bool = false
+//
+//    func userButtonPressed(login: String, password: String) {
+//        if login != User.logins[0].login || password != User.logins[0].password {
+//            statusText.value = "Log in failed."
+//            statusColor.value = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+//            isLoggedIn = false
+//        } else {
+//            statusText.value = "You successfully logged in."
+//            statusColor.value = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+//            isLoggedIn = true
+//        }
+//    }
+//}
